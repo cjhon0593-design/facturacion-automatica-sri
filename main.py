0d5f0b345749c21a283583a08e14506312c361ef
cjhon0593-design <cjhon0593@gmail.com>
2026-05-05 13:02:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from playwright.sync_api import sync_playwright
 import os
+from datetime import datetime
 
 RUC = os.getenv("SRI_RUC")
 CLAVE = os.getenv("SRI_CLAVE")
 
 URL = "https://facturadorsri.sri.gob.ec/portal-facturadorsri-internet/pages/inicio.html"
+
+# DATOS CLIENTE (PRUEBA 1)
+cliente = {
+    "ruc": "1723041156001",
+    "nombre": "PARDO AGURTO GLORIA ALEXANDRA",
+    "subtotal": 230
+}
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -19,18 +27,42 @@ with sync_playwright() as p:
 
     page.wait_for_timeout(5000)
 
-    # IR A EMISIÓN
+    # IR A FACTURA
     page.get_by_role("button", name="Emisión").click()
     page.wait_for_timeout(2000)
-
-    # IR A FACTURA (CORRECTO)
     page.get_by_role("link", name="Factura", exact=True).click()
-    page.wait_for_timeout(8000)
 
-    # DEBUG
-    print("URL actual:", page.url)
-    print("Título actual:", page.title())
-    print("Texto visible:")
-    print(page.locator("body").inner_text())
+    page.wait_for_timeout(6000)
+
+    # =========================
+    # LLENADO DE FACTURA
+    # =========================
+
+    # RUC cliente
+    page.locator("input").nth(2).fill(cliente["ruc"])
+
+    page.wait_for_timeout(2000)
+
+    # Seleccionar tipo RUC
+    page.get_by_text("Seleccione").first.click()
+    page.get_by_text("RUC").click()
+
+    page.wait_for_timeout(2000)
+
+    # Buscar producto
+    page.get_by_placeholder("Buscar en listado de productos").fill("A")
+
+    page.wait_for_timeout(3000)
+
+    page.get_by_text("ASESORIA CONTABILIDAD").click()
+
+    page.wait_for_timeout(2000)
+
+    # Precio unitario
+    page.locator("input").last.fill(str(cliente["subtotal"]))
+
+    page.wait_for_timeout(2000)
+
+    print("FACTURA LLENADA (PRUEBA)")
 
     browser.close()
