3659f93d9c9fb571071064f88a6efaaef2e22d48
cjhon0593-design <cjhon0593@gmail.com>
2026-05-05 13:10:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from playwright.sync_api import sync_playwright
 import os
+from datetime import datetime
 
 RUC = os.getenv("SRI_RUC")
 CLAVE = os.getenv("SRI_CLAVE")
 
 URL = "https://facturadorsri.sri.gob.ec/portal-facturadorsri-internet/pages/inicio.html"
+
+cliente = {
+    "ruc": "1723041156001",
+    "subtotal": 230
+}
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
@@ -23,22 +29,30 @@ with sync_playwright() as p:
     page.get_by_role("link", name="Factura", exact=True).click()
     page.wait_for_timeout(6000)
 
-    # MOSTRAR INPUTS VISIBLES PARA IDENTIFICAR EL CAMPO CORRECTO
-    print("INPUTS VISIBLES:")
-    inputs = page.locator("input:visible")
-    count = inputs.count()
+    # =========================
+    # LLENADO CORRECTO
+    # =========================
 
-    for i in range(count):
-        item = inputs.nth(i)
-        print(
-            i,
-            "type=", item.get_attribute("type"),
-            "name=", item.get_attribute("name"),
-            "id=", item.get_attribute("id"),
-            "placeholder=", item.get_attribute("placeholder"),
-            "value=", item.input_value() if item.get_attribute("type") != "file" else ""
-        )
+    # RUC cliente (INPUT 5)
+    page.locator("input:visible").nth(5).fill(cliente["ruc"])
+    page.wait_for_timeout(3000)
 
-    print("TOTAL INPUTS VISIBLES:", count)
+    # Tipo identificación → RUC
+    page.locator("input:visible").nth(6).click()
+    page.get_by_text("RUC").click()
+    page.wait_for_timeout(2000)
+
+    # Buscar producto
+    page.locator("input:visible").nth(11).fill("A")
+    page.wait_for_timeout(3000)
+
+    page.get_by_text("ASESORIA CONTABILIDAD").click()
+    page.wait_for_timeout(2000)
+
+    # Precio unitario
+    page.locator("input:visible").last.fill(str(cliente["subtotal"]))
+    page.wait_for_timeout(2000)
+
+    print("FACTURA LLENADA CORRECTAMENTE")
 
     browser.close()
